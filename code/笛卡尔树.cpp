f0026f02ad6fb4239378bbe6ff8663ec2cbda853
F. ICE <fyhb233@gmail.com>
2025-08-18 14:43:01 +0800

    Update cartesian tree

--- code/笛卡尔树.cpp
+++ code/笛卡尔树.cpp
@@ -1,7 +1,11 @@
-int lc[N], rc[N], s[N], n, p;
-void build () { // v: i ; w: a[i] ; root=rc[0]
-	for (int i = 1;i <= n;++i) { // 大根堆则是 <
-		while (p && (a[s[p]] > a[i])) p--;
-		lc[i] = rc[s[p]], rc[s[p]] = i, s[++p] = i;
+vector<array<int, 2>> build(vector<int> a, int n) {
+	stack<int> st; st.push(0);
+	vector<array<int, 2>> tree(n + 1);
+	REP(i, 1, n) { // 大根堆则是 <
+		while (ssize(st) > 1 && (a[st.top()] > a[i])) st.pop();
+		tree[i][0] = tree[st.top()][1];
+		tree[st.top()][1] = i;
+		st.push(i);
 	}
+	return tree;
 }
