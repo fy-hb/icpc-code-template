4784fea9c49798b702506489ad7d40187f45a0e7
F. ICE <fyhb233@gmail.com>
2025-08-18 14:42:37 +0800

    Update comb

--- code/组合数.cpp
+++ code/组合数.cpp
@@ -1,11 +1,16 @@
-Mint fac[N], ifac[N];
-void init(int n) {
-	fac[0] = 1;
-	REP(i, 1, n) fac[i] = fac[i-1] * i;
-	ifac[n] = 1 / fac[n];
-	REV(i, n, 1) ifac[i-1] = ifac[i] * i;
+namespace comb {
+vector<Z> fac = {1}, ifac = {1}; int n;
+void init(int m) {
+	if (m < n) return;
+	fac.resize(m+1), ifac.resize(m+1);
+	REP(i, n+1, m) fac[i] = fac[i-1] * i;
+	ifac[m] = 1 / fac[m];
+	REV(i, m, n+1) ifac[i-1] = ifac[i] * i;
+	n = m;
 }
-Mint binom(int x, int y) {
-	if (y > x || y < 0) return 0;
+Z binom(int x, int y) {
+	if (x < y || y < 0) return 0;
+	init(2 * max(x, x-y));
 	return fac[x] * ifac[y] * ifac[x-y];
 }
+};
