f8765b7da1eb599de147cdb1c50e02513558351f
F. ICE <fyhb233@gmail.com>
2025-08-20 22:02:54 +0800

    Update quad residue

--- code/二次剩余.cpp
+++ code/二次剩余.cpp
@@ -1,10 +1,16 @@
+constexpr u64 qpow(u64 x, u64 y, u32 mod) {
+	u64 ans = 1;
+	for (; y; y >>= 1, x *= x, x %= mod)
+		if (y & 1) ans *= x, ans %= mod;
+	return ans;
+}
 constexpr pair<int, int> res2(int n, int p) {
 	if (!n) return {0, 0};
 	if (qpow(n, p / 2, p) != 1) return {-1, -1};
-	i64 t, I; static mt19937_64 rnd(114514);
+	i64 t, I; static mt19937_64 rnd(233);
 	do t = rnd() % p, I = (t * t + p - n) % p;
 	while (qpow(I, p / 2, p) != p - 1);
-	array<i64, 2> ans = {1, 0}, x = {t, 1};
+	array<i64, 2> ans {1, 0}, x {t, 1};
 	auto mul = [&] (array<i64,2> x, array<i64,2> y)->array<i64,2> {
 		return {(x[0] * y[0] + x[1] * y[1] % p * I) % p,
 				(x[0] * y[1] + x[1] * y[0]) % p}; };
