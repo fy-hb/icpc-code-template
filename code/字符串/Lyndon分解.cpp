4feebc9e515000d1941112dcdc0e21844b951b45
F. ICE <fyhb233@gmail.com>
2025-09-06 22:17:04 +0800

    Update lyndon

--- code/字符串/Lyndon分解.cpp
+++ code/字符串/Lyndon分解.cpp
@@ -8,3 +8,15 @@ vector<int> duval(const auto &s) {
 	}
 	return ans;
 } // [ans[i], ans[i+1]), 0-index
+auto minrep(auto s) {
+	int k = 0, i = 0, j = 1, n = ssize(s);
+	while (k < n && i < n && j < n) {
+		if (s[(i + k) % n] == s[(j + k) % n]) k ++;
+		else {
+			(s[(i + k) % n] > s[(j + k) % n] ? i : j) += k + 1;
+			i += (i == j), k = 0;
+		}
+	}
+	rotate(s.begin(), s.begin() + min(i, j), s.end());
+	return s;
+}
