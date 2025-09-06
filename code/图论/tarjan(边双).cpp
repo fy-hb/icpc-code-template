7c4887daf4daa37effa33315bdf18e650a9ae069
F. ICE <fyhb233@gmail.com>
2025-09-06 22:14:59 +0800

    update tarjan(e-bcc)

--- code/图论/tarjan(边双).cpp
+++ code/图论/tarjan(边双).cpp
@@ -1,15 +1,15 @@
-vector<int> bcc[N]; vector<array<int, 2>> adj[N];
+vector<int> bcc[N], adj[N];
 int dfn[N], low[N], st[N], bel[N], top, cnt, tms;
-void tarjan(int u, int in) {
+void tarjan(int u, int p) {
 	low[u] = dfn[u] = ++tms, st[++top] = u;
-	for (auto [v, id] : adj[u]) {
-		if (id == in) continue;
-		if (!dfn[v]) tarjan(v, id), chmin(low[u], low[v]);
+	for (auto v : adj[u]) {
+		if (v == p) { p = 0; continue; }
+		if (!dfn[v]) tarjan(v, u), chmin(low[u], low[v]);
 		else chmin(low[u], dfn[v]);
 	}
 	if (dfn[u] == low[u]) {
 		int z; cnt ++;
-		do z = st[top--], bcc[cnt].push_back(z), bel[z] = cnt;
+		do z = st[top--], bel[z] = cnt, bcc[cnt].push_back(z);
 		while (z != u);
 	}
 }
