a2c0b52e75503805c4f7429fcfd1dca9fa4dcdb8
F. ICE <fyhb233@gmail.com>
2025-09-07 09:52:24 +0800

    fix v-bcc

--- code/图论/tarjan(点双).cpp
+++ code/图论/tarjan(点双).cpp
@@ -1,19 +1,18 @@
-int dfn[N], low[N], st[N], cut[N], top, tms, cnt;
-vector<int> adj[N], bcc[N];
+int dfn[N], low[N], st[N], bel[N], top, tms, cnt;
+vector<int> adj[N], bcc[N]; // bel>1 为割点
 void tarjan(int u, int fa) {
 	dfn[u] = low[u] = ++tms, st[++top] = u; int ch = 0;
 	for (auto v : adj[u]) {
 		if (!dfn[v]) {
 			tarjan(v, u), chmin(low[u], low[v]);
 			if (low[v] >= dfn[u]) {
-				if (++ch > 1 && fa) cut[u] = 1;
-				int z; cnt ++;
-				do z = st[top--], bcc[cnt].push_back(z);
+				int z; cnt ++, ch ++;
+				do z = st[top--], bel[z] ++, bcc[cnt].push_back(z);
 				while (z != v);
-				bcc[cnt].push_back(u);
+				bcc[cnt].push_back(u), bel[u] ++;
 			}
 		}
 		else chmin(low[u], dfn[v]);
 	}
-	if (!fa && !ch) bcc[++cnt] = {u}; // 特判独立点
+	if (!fa && !ch) bcc[++cnt] = {u};
 }
