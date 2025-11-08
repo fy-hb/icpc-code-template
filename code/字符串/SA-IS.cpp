8805c461b39378c2ac0bf9d3ee5d3bc8a437999c
F. ICE <fyhb233@gmail.com>
2025-11-08 23:06:03 +0800

    update sa-is

--- code/字符串/SA-IS.cpp
+++ code/字符串/SA-IS.cpp
@@ -5,7 +5,6 @@ vector<int> sais(const auto& a) { // a[i] >= 1
 	REP(i, 0, n-1) cnt[a[i]] += 1;
 	REP(i, 1, m) cnt[i] += cnt[i-1];
 	REV(i, n-2, 0) typ[i] = a[i]==a[i+1] ? typ[i+1] : (a[i]<a[i+1]);
-
 	auto insort = [&] (vector<int> &lms) {
 		sa.assign(n, -1);
 		int siz = ssize(lms);
@@ -19,20 +18,16 @@ vector<int> sais(const auto& a) { // a[i] >= 1
 		REP(i, 1, m) cur[i] = cnt[i];
 		REV(i, n-1, 0) addS(sa[i]-1);
 	};
-
 	auto check = [&] (int i) -> int { return i==n || (!typ[i-1]&&typ[i]); };
 	auto lmseq = [&] (int i, int j) -> int {
 		do { if (a[i++] != a[j++]) return 0; }
 		while (!check(i) && !check(j));
 		return check(i) && check(j);
 	};
-
 	vector<int> lms;
 	REP(i, 1, n-1) if (check(i)) lms.push_back(i);
 	insort(lms);
-	
 	if (!ssize(lms)) return sa;
-
 	vector<int> mp(n);
 	int cc = 0, last = -1;
 	for (auto v : sa) if (v && check(v)) {
@@ -46,7 +41,7 @@ vector<int> sais(const auto& a) { // a[i] >= 1
 	insort(b);
 	return sa;
 }
-vector<int> geth(const vector<int> &sa, const auto &a) {
+vector<int> getHeight(const vector<int> &sa, const auto &a) {
 	int n = ssize(sa), k = 0;
 	vector<int> rk(n), h(n);
 	REP(i, 0, n-1) rk[sa[i]] = i;
