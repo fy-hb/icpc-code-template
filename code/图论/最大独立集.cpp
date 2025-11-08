dc4d29735abcacbafcb0fec7817bac12143b5393
F. ICE <fyhb233@gmail.com>
2025-11-08 23:06:10 +0800

    update ...

--- code/图论/最大独立集.cpp
+++ code/图论/最大独立集.cpp
@@ -11,23 +11,15 @@ array<u64, 2> dfs(u64 S) {
 	}
 	if (maxd <= 2) {
 		u64 rem = S, sol = 0, cnt = 1;
-
 		REP(_, 0, 1) for (u64 ss = rem; ss; ss &= ss-1) {
 			int s = __builtin_ctzll(ss);
 			if (!(rem >> s & 1)) continue;
 			if (!_ && __builtin_popcountll(ee[s]) > 1) continue;
-
 			front = tail = 0, Q[tail++] = s;
-
-			u64 all = 1ull<<s;
-			u64 select = all;
-			u64 ban = 0ull;
-
+			u64 all = 1ull<<s, select = all, ban = 0ull;
 			while (front < tail) {
 				auto u = Q[front++];
-				if (!(ban >> u & 1)) {
-					select |= 1ull << u, ban |= ee[u];
-				}
+				if (!(ban >> u & 1)) select |= 1ull << u, ban |= ee[u];
 				for (u64 sss = (ee[u] | all) ^ all; sss; sss &= sss - 1) {
 					int v = __builtin_ctzll(sss);
 					Q[tail++] = v, all |= 1ull << v;
@@ -47,6 +39,5 @@ array<u64, 2> dfs(u64 S) {
 	u64 sol = 0, cnt = 0;
 	if (cc1 >= cc0) sol = res1[1] | 1ull << pos, cnt += res1[0];
 	if (cc1 <= cc0) sol = res0[1], cnt += res0[0];
-
 	return {cnt, sol};
 }
