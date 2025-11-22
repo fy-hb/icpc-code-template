4b450725b0d2370b120f50c7abe88bf7ad6c8f19
F. ICE <fyhb233@gmail.com>
2025-11-22 21:50:21 +0800

    update comb
    https://codeforces.com/contest/2171/submission/349953314

--- code/数学/组合数.cpp
+++ code/数学/组合数.cpp
@@ -5,12 +5,12 @@ void init(int m) {
 	fac.resize(m+1), ifac.resize(m+1);
 	REP(i, n+1, m) fac[i] = fac[i-1] * i;
 	ifac[m] = 1 / fac[m];
-	REV(i, m, n+1) ifac[i-1] = ifac[i] * i;
+	REV(i, m, n+2) ifac[i-1] = ifac[i] * i;
 	n = m;
 }
 Z binom(int x, int y) {
 	if (x < y || y < 0) return 0;
-	init(2 * max(x, x-y));
+	if (n < x) init(2 * x);
 	return fac[x] * ifac[y] * ifac[x-y];
 }
 };
